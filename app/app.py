d7123dec58422061a198b3d25f7a922677208eef
Drenii <dreni107@hotmail.com>
2025-12-20 01:05:14 +0100

    adding data

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -22,8 +23,14 @@ async def get_all_posts(limit: int = None):
     return text_posts
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404, detail=f"post with id: {id} was not found")
 
     return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate) -> PostResponse:
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[len(text_posts) + 1] = new_post
+    return new_post
